7282cc96b408c4b2f53e192097d1148f004c69e5
Jason Ball <jason@jasonball.dev>
2021-12-14 01:15:25 -0800

    Add Add, Subtract, Multiply, and Divide operations

--- fraction.go
+++ fraction.go
@@ -1,6 +1,65 @@
 package fraction
 
+import (
+	"fmt"
+)
+
 type Fraction struct {
 	Numerator int
 	Denominator int
+}
+
+func (f *Fraction) String() string {
+	return fmt.Sprintf("%d/%d", f.Numerator, f.Denominator)
+}
+
+func Add(a, b Fraction) Fraction {
+	n := (a.Numerator * b.Denominator) + (a.Denominator * b.Numerator)
+	d := a.Denominator * b.Denominator
+
+	div := GCD(n ,d)
+	n /= div
+	d /= div
+
+	return Fraction{n, d}
+}
+
+func Sub(a, b Fraction) Fraction {
+	n := (a.Numerator * b.Denominator) - (a.Denominator * b.Numerator)
+	d := a.Denominator * b.Denominator
+
+	div := GCD(n ,d)
+	n /= div
+	d /= div
+
+	return Fraction{n, d}
+}
+
+func Mul(a, b Fraction) Fraction {
+	n := a.Numerator * b.Numerator
+	d := a.Denominator * b.Denominator
+
+	div := GCD(n ,d)
+	n /= div
+	d /= div
+
+	return Fraction{n, d}
+}
+
+func Div(a, b Fraction) Fraction {
+	n := a.Numerator * b.Denominator
+	d := a.Denominator * b.Numerator
+
+	div := GCD(n ,d)
+	n /= div
+	d /= div
+
+	return Fraction{n, d}
+}
+
+func GCD(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return GCD(b, a % b)
 }
--- fraction_test.go
+++ fraction_test.go
@@ -19,3 +19,71 @@ func TestFraction(t *testing.T) {
 		t.Errorf("f.Denominator = %d; want %d", f.Denominator, d)
 	}
 }
+
+func TestString(t *testing.T) {
+	f := fraction.Fraction{1, 2}
+	
+	if f.String() != "1/2" {
+		t.Errorf("f.String = \"%s\"; want \"%s\"", f.String(), "1/2")
+	}
+}
+
+
+func TestAdd(t *testing.T) {
+	a := fraction.Fraction{3, 6}
+	b := fraction.Fraction{25, 100}
+
+	r := fraction.Add(a, b)
+	e := fraction.Fraction{3, 4}
+
+	if r != e {
+		t.Errorf("%s + %s = %s; want %s", a.String(), b.String(), r.String(), e.String())
+	}
+}
+
+func TestSub(t *testing.T) {
+	a := fraction.Fraction{3, 6}
+	b := fraction.Fraction{75, 100}
+
+	r := fraction.Sub(b, a)
+	e := fraction.Fraction{1, 4}
+
+	if r != e {
+		t.Errorf("%s + %s = %s; want %s", a.String(), b.String(), r.String(), e.String())
+	}
+}
+
+func TestMul(t *testing.T) {
+	a := fraction.Fraction{3, 6}
+	b := fraction.Fraction{25, 100}
+
+	r := fraction.Mul(a, b)
+	e := fraction.Fraction{1, 8}
+
+	if r != e {
+		t.Errorf("%s + %s = %s; want %s", a.String(), b.String(), r.String(), e.String())
+	}
+}
+
+func TestDiv(t *testing.T) {
+	a := fraction.Fraction{3, 6}
+	b := fraction.Fraction{25, 100}
+
+	r := fraction.Div(a, b)
+	e := fraction.Fraction{2, 1}
+
+	if r != e {
+		t.Errorf("%s + %s = %s; want %s", a.String(), b.String(), r.String(), e.String())
+	}
+}
+
+func TestGCD(t *testing.T) {
+	a := 1071
+	b := 462
+	result := fraction.GCD(a, b)
+	expected := 21
+
+	if result != expected {
+		t.Errorf("GCD(%d, %d) = %d; want %d", a, b, result, expected)
+	}
+}

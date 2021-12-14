f25a8a551d57e57009c583226f89d726452c146d
Jason Ball <jason@jasonball.dev>
2021-12-14 01:34:05 -0800

    Add basic documentation

--- fraction.go
+++ fraction.go
@@ -1,18 +1,24 @@
+// A simple fraction library
 package fraction
 
 import (
 	"fmt"
 )
 
+// A struct for a Fraction.
+// Contains a Numerator and a Denominator.
 type Fraction struct {
 	Numerator int
 	Denominator int
 }
 
+// Returns fractions as strings in the format:
+// "{Numerator}/{Denominator}".
 func (f *Fraction) String() string {
 	return fmt.Sprintf("%d/%d", f.Numerator, f.Denominator)
 }
 
+// Adds two fractions and simplifies the result.
 func Add(a, b Fraction) Fraction {
 	n := (a.Numerator * b.Denominator) + (a.Denominator * b.Numerator)
 	d := a.Denominator * b.Denominator
@@ -24,17 +30,19 @@ func Add(a, b Fraction) Fraction {
 	return Fraction{n, d}
 }
 
+// Subtracts two fractions and simplifies the result.
 func Sub(a, b Fraction) Fraction {
 	n := (a.Numerator * b.Denominator) - (a.Denominator * b.Numerator)
 	d := a.Denominator * b.Denominator
 
-	div := GCD(n ,d)
+	div := GCD(n, d)
 	n /= div
 	d /= div
 
 	return Fraction{n, d}
 }
 
+// Multiplies two fractions and simplifies the result.
 func Mul(a, b Fraction) Fraction {
 	n := a.Numerator * b.Numerator
 	d := a.Denominator * b.Denominator
@@ -46,6 +54,7 @@ func Mul(a, b Fraction) Fraction {
 	return Fraction{n, d}
 }
 
+// Divides two fractions and simplifies the result.
 func Div(a, b Fraction) Fraction {
 	n := a.Numerator * b.Denominator
 	d := a.Denominator * b.Numerator
@@ -57,6 +66,9 @@ func Div(a, b Fraction) Fraction {
 	return Fraction{n, d}
 }
 
+// Computes the greatest common divisor between two
+// integers.
+// Algorithm from https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations.
 func GCD(a, b int) int {
 	if b == 0 {
 		return a
